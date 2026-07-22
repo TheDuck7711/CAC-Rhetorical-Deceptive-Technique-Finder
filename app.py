6f09820184f4d030f94acdf4113c88903244690d
TheDuck7711 <jpvgoblue@gmail.com>
2026-07-22 13:52:40 -0500

    developed home and analyze html and routed to them in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,11 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/analyze", methods=["post"])
+def analyze():
+    input_text = request.form["input_text"]
+    word_count = len(input_text.split())
+    return render_template("analyze.html",text=input_text, word_count = word_count)
+
 @app.route("/banana")
 def banana():
     return "Banana"
 
-from analyzer import load_AFINN, load_rhetoric_words
+from loader import load_AFINN, load_rhetoric_words
 
 lexicon = load_AFINN()
 rhetoric_words = load_rhetoric_words()
