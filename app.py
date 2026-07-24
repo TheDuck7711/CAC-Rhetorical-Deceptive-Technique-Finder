bac38d6b2e5e75b6b734b07b775776c4284cad1b
TheDuck7711 <jpvgoblue@gmail.com>
2026-07-24 11:45:24 -0500

    connected the analyzer functions to the route. accessed it  in analyze.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from analyzers import rhetoric_spotter, sentiment_analysis
 
 app = Flask(__name__)
 
@@ -9,8 +10,13 @@ def home():
 @app.route("/analyze", methods=["post"])
 def analyze():
     input_text = request.form["input_text"]
+    sentiment_analysis_results = sentiment_analysis(input_text)
+    rhetoric_spotter_results = rhetoric_spotter(input_text)
     word_count = len(input_text.split())
-    return render_template("analyze.html",text=input_text, word_count = word_count)
+    return render_template("analyze.html",text=input_text, word_count = word_count, abs_avg = sentiment_analysis_results[0],
+                            net_avg = sentiment_analysis_results[1], range_high = sentiment_analysis_results[2],
+                            range_low = sentiment_analysis_results[3], numb_rhetoric = rhetoric_spotter_results[0], percent_rhetoric = rhetoric_spotter_results[1],
+                            list_rhetoric = rhetoric_spotter_results[2])
 
 @app.route("/banana")
 def banana():
