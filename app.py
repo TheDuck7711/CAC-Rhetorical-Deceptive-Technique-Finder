ebf8b517eea64854c78b1feaa977fba27771a353
TheDuck7711 <jpvgoblue@gmail.com>
2026-07-27 14:26:22 -0500

    changed the the method used for the rhetorical word finder function in analyzers.py and made changes to the html using jinja

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
-from analyzers import rhetoric_spotter, sentiment_analysis
+from analyzers import rhetoric_spotter, sentiment_analysis,rhetoric_spotter_v2
 
 app = Flask(__name__)
+
+from loader import load_AFINN, load_rhetoric_words
+
+lexicon = load_AFINN()
+rhetoric_words = load_rhetoric_words()
 
 @app.route("/")
 def home():
@@ -18,11 +23,18 @@ def analyze():
                             range_low = sentiment_analysis_results[3], numb_rhetoric = rhetoric_spotter_results[0], percent_rhetoric = rhetoric_spotter_results[1],
                             list_rhetoric = rhetoric_spotter_results[2])
 
+@app.route("/analyze_v2", methods=["post"])
+def analyze_v2():
+    input_text = request.form["input_text"]
+    sentiment_analysis_results = sentiment_analysis(input_text)
+    rhetoric_spotter_results = rhetoric_spotter_v2(input_text)
+    word_count = len(input_text.split())
+    return render_template("analyze_v2.html",text=input_text, word_count = word_count, abs_avg = sentiment_analysis_results[0],
+                            net_avg = sentiment_analysis_results[1], range_high = sentiment_analysis_results[2],
+                            range_low = sentiment_analysis_results[3], numb_rhetoric = rhetoric_spotter_results[0], percent_rhetoric = rhetoric_spotter_results[1],
+                            list_rhetoric = rhetoric_spotter_results[2])
+
 @app.route("/banana")
 def banana():
     return "Banana"
 
-from loader import load_AFINN, load_rhetoric_words
-
-lexicon = load_AFINN()
-rhetoric_words = load_rhetoric_words()
